62703af122627c83740a4f1330b019a5ac3377a4
Camionsauro <maxbenini@gmail.com>
2024-03-29 09:42:23 +0100

    final

---
+++ myscript.js
@@ -0,0 +1,29 @@
+$("#slideshow > div:gt(0)").hide();
+
+let delay = [50000, 5000, 5000, 5000, 5000, 5000, 5000, 5000, 5000, 5000, 5000, 5000], i = 0;
+
+function slide() {
+    setTimeout(() => {
+        effect();
+        if(i === delay.length){
+            i = 0;
+            console.log('Next delay: ' + delay[i] + ' ms');
+        }
+        else{
+            console.log('Next delay: ' + delay[i] + ' ms');
+        }
+        slide();
+    }, delay[i])
+    i++;
+}
+
+slide();
+
+function effect(){
+    $('#slideshow > div:first')
+        .fadeOut(1000)
+        .next()
+        .fadeIn(1000);
+
+    setTimeout(() => {$('#slideshow > div:first').appendTo("#slideshow")}, 1000)
+}
